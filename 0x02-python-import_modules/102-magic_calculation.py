a86197ec82422e8fce4e407f8038daf6cad99001
tolossamuel <tolossamuel1@gmail.com>
2023-10-05 17:41:22 +0300

    pycodestyle

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from magic_calculation_102 import add, sub
+
+
 def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
@@ -8,4 +10,4 @@ def magic_calculation(a, b):
         return (c)
 
     else:
-        return(sub(a, b))
+        return (sub(a, b))
