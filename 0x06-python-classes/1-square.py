1df0140ddfecb051abe1ea20d63774fa6d7544ba
tolossamuel <tolossamuel1@gmail.com>
2023-10-24 22:03:17 +0300

    updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
-"""a class Square. with methods"""
+"""Define a class Square."""
 
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size):
+        """Initialize a new Square.
+            size (int): The size of the new square.
+        """
         self.__size = size
