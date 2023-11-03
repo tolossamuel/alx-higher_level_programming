4659e69debc024d37036bf019fc956e64fc20b21
tolossamuel <tolossamuel1@gmail.com>
2023-11-03 23:22:09 +0300

    task 6

--- 0x07-python-test_driven_development/ 6-max_integer.py
+++ 0x07-python-test_driven_development/ 6-max_integer.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-"""Defines a text-indentation function."""
+"""Module to find the max integer in a list
+"""
 
 
-def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-    Raises:
-        TypeError: If text is not a string.
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
     """
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
-
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
