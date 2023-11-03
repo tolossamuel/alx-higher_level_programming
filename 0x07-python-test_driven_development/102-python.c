fa3e47b0fe5b634f2a124333e0b3504fed52f132
tolossamuel <tolossamuel1@gmail.com>
2023-11-03 23:22:59 +0300

    advance task 3

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,8 +1,8 @@
 #include "Python.h"
 
 /**
- * print_python_string - Prints
- * @p: PyObject object.
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
  */
 void print_python_string(PyObject *p)
 {
