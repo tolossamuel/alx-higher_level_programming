2dbebf8c1debf92d92ed4161f3b0c585d6166ad9
tolossamuel <tolossamuel1@gmail.com>
2023-11-06 18:43:07 +0300

    task 4

---
+++  0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines an inherited class-checking function."""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
